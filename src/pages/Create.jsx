28b3618e8a31dd14b4015b3d5ccdf9b701e88b8d
Dawson02 <127239947+Dawson02@users.noreply.github.com>
2023-08-10 12:34:38 -0230

    Update Create.jsx

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,6 +1,5 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Header from "../components/Header";
-
 
 function Create() {
     const [serviceName, setServiceName] = useState('');
@@ -9,72 +8,120 @@ function Create() {
     const [servicePrice, setServicePrice] = useState('');
     const [serviceLocation, setServiceLocation] = useState('');
     const [serviceImages, setServiceImages] = useState([]);
+    const [submitMessage, setSubmitMessage] = useState('');
 
     const handleImageUpload = (event) => {
         // Handle the image upload
     }
 
-    const handleSubmit = (event) => {
+    const handleSubmit = async (event) => {
         event.preventDefault();
+
+        const newService = {
+            name: serviceName,
+            description: serviceDescription,
+            category: serviceCategory,
+            price: servicePrice,
+            location: serviceLocation
+        };
+
+        try {
+            const response = await fetch('http://localhost:5000/services', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(newService)
+            });
+        
+            console.log('Response status:', response.status);
+            console.log('Response data:', await response.json());
+        
+            if (response.ok) {
+                setSubmitMessage('Service created successfully!');
+            } else {
+                setSubmitMessage('Failed to create service.');
+            }
+        } catch (error) {
+            console.error('Error creating service:', error);
+            setSubmitMessage('An error occurred.');
+        }
+        
+
+        // Clear input fields
+        setServiceName('');
+        setServiceDescription('');
+        setServiceCategory('');
+        setServicePrice('');
+        setServiceLocation('');
     }
 
-    return(
+    return (
         <div>
-
             <Header />
-
-            <div class="create-container">
-                <div class="create-h1">
+            <div className="create-h1">
                 <h1>Create A Service!</h1>
-                </div>
-
-                <div class="create-form">
+            </div>
+            <div className="create-container">
+                <div className="create-form">
                     <form onSubmit={handleSubmit}>
-                        <label>
-                            Service Name:
-                            <input 
-                                type="text"
-                                value=""
-                                onChange={(e) => setServiceName(e.target.value)} required
-                            />
-                        </label>
+                        <label>Service Name:</label>
+                        <input
+                            type="text"
+                            id="serviceName"
+                            value={serviceName}
+                            onChange={(e) => setServiceName(e.target.value)}
+                            required
+                        />
 
                         <label>
                             Service Description:
-                            <input 
+                            <input
                                 type="text"
-                                value=""
-                                onChange={(e) => setServiceDescription(e.target.value)} required
+                                id="serviceDescription"
+                                value={serviceDescription}
+                                onChange={(e) => setServiceDescription(e.target.value)}
+                                required
                             />
                         </label>
 
                         <label>
                             Service Category:
-                            <input 
+                            <input
                                 type="text"
-                                value=""
-                                onChange={(e) => setServiceCategory(e.target.value)} required
+                                id="serviceCategory"
+                                value={serviceCategory}
+                                onChange={(e) => setServiceCategory(e.target.value)}
+                                required
                             />
                         </label>
 
                         <label>
                             Service Price:
-                            <input 
+                            <input
                                 type="text"
-                                value=""
-                                onChange={(e) => setServicePrice(e.target.value)} required
+                                id="servicePrice"
+                                value={servicePrice}
+                                onChange={(e) => setServicePrice(e.target.value)}
+                                required
                             />
                         </label>
 
                         <label>
                             Service Location:
-                            <input 
+                            <input
                                 type="text"
-                                value=""
-                                onChange={(e) => setServiceLocation(e.target.value)} required
+                                id="serviceLocation"
+                                value={serviceLocation}
+                                onChange={(e) => setServiceLocation(e.target.value)}
+                                required
                             />
                         </label>
+                        <button type="submit" className="create-button">Create Service</button>
                     </form>
+                    <div className="submit-message">
+                        {submitMessage && <p>{submitMessage}</p>}
+                    </div>
                 </div>
             </div>
         </div>
