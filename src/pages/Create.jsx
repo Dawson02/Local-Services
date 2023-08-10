56bfcd260dcb7de76ced76ef5263a5573b7363b2
Dawson02 <127239947+Dawson02@users.noreply.github.com>
2023-08-10 17:20:52 -0230

    Update Create.jsx

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Header from "../components/Header";
+import Footer from "../components/Footer"
 
 function Create() {
     const [serviceName, setServiceName] = useState('');
@@ -123,6 +124,9 @@ function Create() {
                         {submitMessage && <p>{submitMessage}</p>}
                     </div>
                 </div>
+            </div>
+            <div class="footer">
+                <Footer />
             </div>
         </div>
     )
