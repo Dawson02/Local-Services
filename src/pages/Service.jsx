db6d15ccaca3c1508da8e6af3ebf8d94cc830eea
BradenSkiffington <127122436+BradenSkiffington@users.noreply.github.com>
2023-08-10 16:31:31 -0230

    Update Service.jsx

--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -1,39 +1,40 @@
-import Header from "../components/Header"
-import React from 'react';
-import useFetchServices from '../components/useFetchServices';
+import Header from "../components/Header";
+import React from "react";
+import useFetchServices from "../components/useFetchServices";
+import "./Service.css";
 
 function Service() {
-    const { services, loading, error } = useFetchServices();
+  const { services, loading, error } = useFetchServices();
 
-    if (loading) {
-        return <p>Loading...</p>;
-    }
+  if (loading) {
+    return <p>Loading...</p>;
+  }
 
-    if (error) {
-        return <p>Error: {error}</p>;
-    }
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
 
-    return (
-        <div>
-            <Header />
-            <div class="service-h1">
-                <h1>Services</h1>
-            </div>
-            <div class="service-listing">
-                <ul>
-                    {services.map(service => (
-                        <li key={service.id}>
-                            <h2>{service.name}</h2>
-                            <p>{service.description}</p>
-                            <p>{service.category}</p>
-                            <p>{service.price}</p>
-                            <p>{service.location}</p>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      <div className="header">
+        <h1 className="header-title">Services</h1>
+      </div>
+      <div className="service-container">
+        <ul className="service-listing">
+          {services.map((service) => (
+            <li key={service.id} className="service-box">
+              <h2>{service.name}</h2>
+              <p className="service-details">{service.description}</p>
+              <p className="service-details">{service.category}</p>
+              <p className="service-details">{service.price}</p>
+              <p className="service-details">{service.location}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Service;
