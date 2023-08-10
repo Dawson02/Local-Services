834e815c7f14e2fe045754c5c8e32d9f7563c83a
Dawson02 <127239947+Dawson02@users.noreply.github.com>
2023-08-10 12:35:51 -0230

    Add files via upload

---
+++ src/data/ServiceData.js
@@ -0,0 +1,28 @@
+const ServiceData = [
+    {
+      id: 1,
+      serviceName: "Service 1",
+      serviceDescription: "Service 1 Description",
+      serviceCategory: "Service 1 Category",
+      servicePrice: "Service 1 Price",
+      serviceLocation: "Service 1 Location",
+    },
+
+    {
+      id: 2,
+      serviceName: "Service 2",
+      serviceDescription: "Service 2 Description",
+      serviceCategory: "Service 2 Category",
+      servicePrice: "Service 2 Price",
+      serviceLocation: "Service 2 Location",
+    },
+
+    {
+      id: 1,
+      serviceName: "Service 3",
+      serviceDescription: "Service 3 Description",
+      serviceCategory: "Service 3 Category",
+      servicePrice: "Service 3 Price",
+      serviceLocation: "Service 3 Location",
+    },
+]
